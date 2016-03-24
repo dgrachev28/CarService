37eba6da4e1ad95f945e3a30d841c19a5cdae57a
marijasmolina <marija_smolina@mail.ru>
2016-03-25 00:58:36 +0300

    Added updating content on the page

--- src/main/webapp/WEB-INF/pages/scripts/main.js
+++ src/main/webapp/WEB-INF/pages/scripts/main.js
@@ -1,33 +1,111 @@
 $(document).ready(function() {
 
-    // Константы
-    var TIMER_DELAY = 5000;
+    var classes = {
+        contentButton: "content__button",
+        contentButtonPlay: "content__button_play",
+        workshop: "workshop",
+        workshopMasters: "workshop-masters",
+        workshopMastersItem: "workshop-masters__item",
+        workshopQueue: "workshop-queue",
+        workshopQueueItem: "workshop-queue__item"
+    };
 
-    // Переменные
-    var timerId;
+
+
+    var constants = {
+        TIMER_DELAY: 5000
+    };
 
 
 
-    //Обработчики событий
+    var timerId,
+        $contentButton,
+        $workshop;
 
-    // Пауза / запуск
-    $(".content__button").on("click", function() {
+
+
+
+    function initVars() {
+        $contentButton = $("." + classes.contentButton);
+        $workshop = $("." + classes.workshop);
+    }
+
+
+    function bindEvents() {
+        $contentButton.on("click", playButtonHandler);
+    }
+
+
+
+    function playButtonHandler() {
         var $this = $(this);
-        if ($this.hasClass("content__button_play")) {
-            $this.removeClass("content__button_play");
+        if ($this.hasClass(classes.contentButtonPlay)) {
+            $this.removeClass(classes.contentButtonPlay);
             timerStart();
         } else {
-            $this.addClass("content__button_play");
+            $this.addClass(classes.contentButtonPlay);
             clearTimeout(timerId);
         }
-    });
+    }
 
 
 
-    // Обновление данных на главной странице
-    function updateMainPage(data) {
-        $(".masha").val(data);
-        console.log(data);
+    function updateContent(data) {
+        var i, j;
+
+        // Removing content
+        $("." + classes.workshopQueueItem).remove();
+        $("." + classes.workshopMastersItem).remove();
+
+
+
+        if (data && data.workshops) {
+            var workshops = data.workshops;
+            for (i = 0; workshops && i < workshops.length; ++i) {
+                var id = workshops[i].id;
+
+                var markupWorkshop = $("#workshop" + id);
+
+                // Update queue
+                var queue = workshops[i].queue;
+                var markupQueue = markupWorkshop.find("." + classes.workshopQueue);
+                for (j = 0; j < queue.length; ++j) {
+                    markupQueue.append(getQueueItemMarkup(queue[j].id, queue[j].number, queue[j].service, queue[j].date));
+                }
+
+                //Update masters
+                var masters = workshops[i].masters;
+                var markupMasters = markupWorkshop.find("." + classes.workshopMasters);
+                for (j = 0; j < masters.length; ++j) {
+                    markupMasters.append(getMasterMarkup(masters[j].id, masters[j].name, masters[j].busy));
+                }
+            }
+        }
+    }
+
+    // Helpers
+    function getQueueItemMarkup(id, number, service, date) {
+        return '<li class="workshop-queue__item" id="ticket' + id + '">' +
+            '<span class="workshop-queue__item-number">' + number + '</span>' +
+            '<span class="workshop-queue__item-service">' + service + '</span>' +
+            '<span class="workshop-queue__item-date">' + date + '</span>' +
+        '</li>';
+    }
+
+    function getMasterMarkup(id, name, busy) {
+        function busyMarkup(busy) {
+            if(busy) {
+                return '<span class="workshop-masters__item-work">занят</span>';
+            } else {
+                return '<span class="workshop-masters__item-work workshop-masters__item-work_free">свободен</span>';
+            }
+        }
+
+        return '<li class="workshop-masters__item" + id="master' + id + '">' +
+            '<span class="workshop-masters__item-name">' + name + '</span>' +
+            '<span class="workshop-masters__item-work">' + busyMarkup(busy) + '</span>' +
+            '</li>';
+
     }
 
 
@@ -39,7 +117,7 @@ $(document).ready(function() {
             url: '/getCurrentState',
             data: {},
             success: function(data) {
-                updateMainPage(data);
+                updateContent(data);
             }
         });
     }
@@ -49,13 +127,174 @@ $(document).ready(function() {
     function timerStart() {
         timerId = setTimeout(function tick() {
             receive();
-            timerId = setTimeout(tick, TIMER_DELAY);
-        }, TIMER_DELAY);
+            timerId = setTimeout(tick, constants.TIMER_DELAY);
+        }, constants.TIMER_DELAY);
+    }
+
+
+
+    function init() {
+
+        initVars();
+
+        bindEvents();
+
+        updateContent({
+            workshops: [
+                {
+                    id: 3,
+                    queue: [],
+                    masters: [
+                        {
+                            id: 17,
+                            name: "Денис",
+                            busy: false
+                        },
+                        {
+                            id: 9,
+                            name: 'Анна',
+                            busy: true
+                        },
+                        {
+                            id: 7,
+                            name: "Сергей",
+                            busy: true
+                        }
+                    ]
+                },
+                {
+                    id: 1,
+                    queue: [
+                        {
+                            id: 33,
+                            number: "А789ОНР",
+                            service: 'техосмотр',
+                            date: '10:33 24/03'
+                        }
+                    ],
+                    masters: []
+                },
+                {
+                    id: 2,
+                    queue: [
+                        {
+                            id: 1,
+                            number: "Р777ПЕ",
+                            service: 'покраска кузова',
+                            date: '12:46 24/03'
+                        }
+                    ],
+                    masters: []
+                },
+                {
+                    id: 4,
+                    queue: [
+                        {
+                            id: 53,
+                            number: "П411РН",
+                            service: 'ремонт двигателя',
+                            date: '12:46 24/03'
+                        },
+                        {
+                            id: 54,
+                            number: "Н573ТЗ",
+                            service: 'ремонт двигателя',
+                            date: '15:23 24/03'
+                        },
+                        {
+                            id: 55,
+                            number: "П411РН",
+                            service: 'замена масла',
+                            date: '15:48 24/03'
+                        }
+                    ],
+                    masters: []
+                }
+            ]
+        });
+
+
+        updateContent({
+            workshops: [
+                {
+                    id: 3,
+                    queue: [],
+                    masters: [
+                        {
+                            id: 17,
+                            name: "Денис",
+                            busy: true
+                        },
+                        {
+                            id: 9,
+                            name: 'Анна',
+                            busy: false
+                        },
+                        {
+                            id: 7,
+                            name: "Сергей",
+                            busy: false
+                        }
+                    ]
+                },
+                {
+                    id: 1,
+                    queue: [],
+                    masters: []
+                },
+                {
+                    id: 2,
+                    queue: [
+                        {
+                            id: 1,
+                            number: "Р777ПЕ",
+                            service: 'ремонт кузова',
+                            date: '12:46 24/03'
+                        }
+                    ],
+                    masters: []
+                },
+                {
+                    id: 4,
+                    queue: [
+                        {
+                            id: 53,
+                            number: "П411РН",
+                            service: 'ремонт двигателя',
+                            date: '12:46 24/03'
+                        },
+                        {
+                            id: 54,
+                            number: "Н573ТЗ",
+                            service: 'замена масла',
+                            date: '15:23 24/03'
+                        },
+                        {
+                            id: 55,
+                            number: "П411РН",
+                            service: 'замена масла',
+                            date: '15:48 24/03'
+                        }
+                    ],
+                    masters: []
+                }
+            ]
+        });
+
+
+        //$.ajax({
+        //    method: 'POST',
+        //    url: '/getCurrentState',
+        //    data: {},
+        //    success: function(data) {
+        //        updateContent(data);
+        //    }
+        //});
     }
 
 
 
     // Начало работы
-    timerStart();
+    init();
 
 });
